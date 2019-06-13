8d0bfc5ddf35dc2bae167816e4e906e0ef35a3a3
Tyler Hill <t.wakefield.hill@gmail.com>
2019-06-12 21:59:32 -0400

    js updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,8 +23,26 @@ function newWord() {
         return Math.floor(min + Math.random()*(max + 1 - min))
     }
 
+    // pulls the element with that index from the array
     var number = generateRandomInteger(min, max);
+    var currentWord = words[number];
+    console.log(currentWord);
 
+    // display the _ _ _ ... according to the number of letters in the word
+    for (i=0; i<currentWord.length; i++) {
+        document.getElementById("currentWord").innerHTML = document.getElementById("currentWord").innerHTML + "_ "
+    }
+
+    return currentWord;
 }
 
-newWord();
+var currentWord = newWord();
+console.log("This is the current word: " + currentWord)
+
+// letter checking function
+document.onkeyup = function(event) {
+
+    var letter = event.key.toLowerCase();
+    var correctLetterCounter = 0;
+
+}
