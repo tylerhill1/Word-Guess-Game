739a44f09a4eba7384906459329ec58e8860fa9f
Tyler Hill <t.wakefield.hill@gmail.com>
2019-06-14 11:45:47 -0400

    keep track of incorrect guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,6 +14,7 @@ var words = [
 var lengthOfGame = words.length;
 var tracker = 0;
 var wins = 0;
+var incorrectGuessesRemaining = 6;
 
 var correctLetterCounter;
 
@@ -72,37 +73,37 @@ document.onkeyup = function(event) {
     
     var letter = event.key.toLowerCase();
     var j = 0;
-    // document.getElementById("currentWord").innerHTML = "";
+    var k = 0;
+    console.log("Does this work? " + currentWord);
+
+    for(i=0; i<currentWord.length; i++) {
+        
+        if(letter === currentWord.charAt(i)) {
+            k++;
+        }
+        
+    }
+
+    if(k===0) {
+        incorrectGuessesRemaining--;
+        document.getElementById("guesses").innerHTML = incorrectGuessesRemaining;
+    }
+    
     for(i=0; i<displayedWord.length; i++) {
         
         if((letter + " ") === displayedWord[i]) {
             j++;
         }
+        
     }
     console.log(currentWord.length);
     
     if (j === 0) {
+
         for(i=0; i<currentWord.length; i++) {
         var correctLetter = currentWord.charAt(i);
         console.log("correct letter: " + correctLetter);
         console.log("letter: " + letter);
-        
-        // if(letter === correctLetter) {
-        //     var test = i*2;
-        //     console.log("test: " + test);
-        //     var dashesWord = document.getElementById("currentWord").innerHTML;
-        //     console.log(dashesWord);
-        //     var letterInQuestion = dashesWord.charAt(test);
-        //     console.log(letterInQuestion);
-
-        //     var res = dashesWord.replace(letterInQuestion,letter);
-        //     console.log(res);
-        //     document.getElementById("currentWord").innerHTML = res;
-        //     // word.replace(word.charAt(i*2),letter);
-        //     // console.log(word);
-        //     // document.getElementById("currentWord").innerHTML = word;
-        //     correctLetterCounter++;
-        // }
 
         if(letter === correctLetter) {
             spliceArray(i, letter + " ");
@@ -128,108 +129,4 @@ document.onkeyup = function(event) {
         document.getElementById("wins").innerHTML = wins;
         }
 }
-
-
-
-// var words = [
-//     "rowing",
-//     "curling",
-//     "tennis",
-//     "gymnastics",
-//     "badmitton",
-//     "basketball",
-//     "volleyball",
-//     "fencing",
-//     "rugby",
-//     "cycling",
-//     "weightlifting",
-// ]
-
-// console.log(words);
-
-// function newWord() {
-//     var min = 0;
-//     var max = words.length - 1;
-    
-//     // generates random number
-//     function generateRandomInteger (min , max) {
-//         return Math.floor(min + Math.random()*(max + 1 - min))
-//     }
-
-//     // pulls the element with that index from the array
-//     var number = generateRandomInteger(min, max);
-//     var currentWord = words[number];
-//     console.log(currentWord);
-    
-//     return currentWord;
-// }
-
-// var currentWord = newWord();
-// console.log(currentWord);
-// var displayedWord = []
-
-// function displayNewWord() {
-//     document.getElementById("currentWord").innerHTML = "";
-//     for(i=0; i<currentWord.length; i++) {
-//         document.getElementById("currentWord").innerHTML = document.getElementById("currentWord").innerHTML + displayedWord[i];   
-//     }
-//     return finalWord = document.getElementById("currentWord").innerHTML;
-// }
-// function displayWord() {
-//     // display the _ _ _ ... according to the number of letters in the word
-//     for (i=0; i<currentWord.length; i++) {
-//         displayedWord.push("_ ");
-//     }
-    
-//     return finalWord = displayNewWord();
-// }
-
-// displayWord();
-
-// console.log("This is the current word: " + finalWord)
-
-// function spliceArray(x, y) {
-//     displayedWord.splice(x, 1, y);
-//   }
-
-// // letter checking function
-// document.onkeyup = function(event) {
-    
-//     var letter = event.key.toLowerCase();
-//     var correctLetterCounter = 0;
-//     // document.getElementById("currentWord").innerHTML = "";
-
-//     console.log(currentWord.length);
-//     for(i=0; i<currentWord.length; i++) {
-//         var correctLetter = currentWord.charAt(i);
-//         console.log("correct letter: " + correctLetter);
-//         console.log("letter: " + letter);
-        
-//         // if(letter === correctLetter) {
-//         //     var test = i*2;
-//         //     console.log("test: " + test);
-//         //     var dashesWord = document.getElementById("currentWord").innerHTML;
-//         //     console.log(dashesWord);
-//         //     var letterInQuestion = dashesWord.charAt(test);
-//         //     console.log(letterInQuestion);
-
-//         //     var res = dashesWord.replace(letterInQuestion,letter);
-//         //     console.log(res);
-//         //     document.getElementById("currentWord").innerHTML = res;
-//         //     // word.replace(word.charAt(i*2),letter);
-//         //     // console.log(word);
-//         //     // document.getElementById("currentWord").innerHTML = word;
-//         //     correctLetterCounter++;
-//         // }
-
-//         if(letter === correctLetter) {
-//             spliceArray(i, (letter + " "));
-//             correctLetterCounter++;
-//         }
-
-//         if(correctLetterCounter === currentWord.length) {
-//             newWord();
-//         }
-//     }
-// }
 
